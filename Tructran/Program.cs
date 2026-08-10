45e349a337969ac71ec9b4f6332b4eccda73249f
UEH <UEH@B507PC19>
2026-08-10 16:46:42 +0700

    Add project files.

---
+++ Tructran/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hallo heheheh");
+Console.WriteLine("Hello, World!");
+
