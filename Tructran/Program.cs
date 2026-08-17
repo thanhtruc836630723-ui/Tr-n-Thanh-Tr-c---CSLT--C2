bb80c1e3781937fd068e2168ad8c99e238536b26
truc <thanhtruc836630723@gmail.com>
2026-08-17 15:12:05 +0700

    nd

--- Tructran/Program.cs
+++ Tructran/Program.cs
@@ -2,10 +2,8 @@
 using System.Xml;
 
 Console.WriteLine("oohohoohohhh");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
 Console.WriteLine("hong nhan bac phan song gio");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("hallo heheheh");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("my name is Youky");
 
